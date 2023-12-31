389f3d0b33dab373a0ea6bc77093ef2c04153c70
SING TI NG <ngsingti1654@gmail.com>
2023-12-31 19:31:41 +0800

    Code And Coffee

--- app/src/main/java/com/example/codeandcoffee/LoginActivity.java
+++ app/src/main/java/com/example/codeandcoffee/LoginActivity.java
@@ -1,19 +1,14 @@
 package com.example.codeandcoffee;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
-
 }
