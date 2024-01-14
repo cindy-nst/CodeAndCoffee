164eda69c1c9d567acb13c1719a62fe6466c19fb
SING TI NG <ngsingti1654@gmail.com>
2024-01-14 21:58:38 +0800

    Update Code Account Activity xml

--- app/src/main/java/com/example/codeandcoffee/AccountActivity.java
+++ app/src/main/java/com/example/codeandcoffee/AccountActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar_account);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(Boolean.TRUE);
+            getSupportActionBar().setTitle("Account");
+        }
     }
 }
