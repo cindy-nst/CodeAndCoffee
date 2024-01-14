d9f1b3688db26361b123c4ca20750f18cea221dc
SING TI NG <ngsingti1654@gmail.com>
2024-01-14 22:34:55 +0800

    Update codes in PickUp Acitivty, HelpCentre Activity, Order Row xml, Home Fragment

--- app/src/main/java/com/example/codeandcoffee/HelpCentreActivity.java
+++ app/src/main/java/com/example/codeandcoffee/HelpCentreActivity.java
@@ -8,22 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class HelpCentreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_centre);
-//        @SuppressLint("WrongViewCast")
-//        RelativeLayout chatWithAdminLayout = findViewById(R.id.linearLayoutChatWithAdmin);
-//        chatWithAdminLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Open a new activity (intent) for chatting with admin
-//                Intent intent = new Intent(HelpCentreActivity.this, ChatWithBotActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar_HelpCentre);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(Boolean.TRUE);
+            getSupportActionBar().setTitle("Help Center");
+        }
 
         @SuppressLint("WrongViewCast")
         RelativeLayout contactUsLayout = findViewById(R.id.linearLayoutContactUs);
--- app/src/main/java/com/example/codeandcoffee/PickupActivity.java
+++ app/src/main/java/com/example/codeandcoffee/PickupActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class PickupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pickup);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar_pickup);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(Boolean.TRUE);
+            getSupportActionBar().setTitle("Pick Up");
+        }
     }
 }
