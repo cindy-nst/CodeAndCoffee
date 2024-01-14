faf1809f96b9b93caf81e765179f34d9706dfee5
SING TI NG <ngsingti1654@gmail.com>
2024-01-14 12:28:00 +0800

    Update Order Activity and Order xml codes

--- app/src/main/java/com/example/codeandcoffee/OrderActivity.java
+++ app/src/main/java/com/example/codeandcoffee/OrderActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar_order);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Order");
+        }
+
     }
 }
